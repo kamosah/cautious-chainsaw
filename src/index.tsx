9981db7402e672b1100df712c98c7623a577cd34
kamosah <68175563+kamosah@users.noreply.github.com>
2022-07-10 15:54:04 -0400

    Adding react-dom types

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-// @ts-expect-error TS(7016) FIXME: Could not find a declaration file for module 'reac... Remove this comment to see the full error message
 import { render } from 'react-dom'
 import {StoreProvider} from './store'
 import 'todomvc-app-css/index.css'
