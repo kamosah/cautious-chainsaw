ecdf4bf80eb3020c568a9075773a5d37bfb7003a
kamosah <68175563+kamosah@users.noreply.github.com>
2022-07-10 15:48:39 -0400

    Adding react-test-renderer types

--- src/components/MainSection.spec.tsx
+++ src/components/MainSection.spec.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-// @ts-expect-error TS(2307) FIXME: Cannot find module 'react-test-renderer/shallow' o... Remove this comment to see the full error message
 import { createRenderer } from 'react-test-renderer/shallow';
 import MainSection from './MainSection'
 import Footer from './Footer'
--- src/components/TodoList.spec.tsx
+++ src/components/TodoList.spec.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-// @ts-expect-error TS(2307) FIXME: Cannot find module 'react-test-renderer/shallow' o... Remove this comment to see the full error message
 import { createRenderer } from 'react-test-renderer/shallow';
 import TodoList from './TodoList'
 import TodoItem from './TodoItem'
