e900bc37cf3ab20f7eed69aea65b9ea490804cfe
kamosah <68175563+kamosah@users.noreply.github.com>
2022-07-17 16:17:31 -0400

    Adding intial typescript configurations

---
+++ tsCoverage.ts
@@ -0,0 +1,19 @@
+import glob from "glob";
+
+let tsCoverage = 0;
+
+const getCoverage = async () => {
+  const allJSTSFiles = glob.sync("/**/*.[j|t]s?(x)", {
+    root: "src",
+  });
+  const tsFiles = allJSTSFiles.filter(
+    (file) => file.endsWith("ts") || file.endsWith("tsx")
+  );
+  tsCoverage = +(tsFiles.length / allJSTSFiles.length).toFixed(2);
+  return tsCoverage;
+};
+
+getCoverage().then((coverage) => {
+  console.log("10");
+  return coverage;
+});
